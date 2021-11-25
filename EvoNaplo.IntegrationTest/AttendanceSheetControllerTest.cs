26cad3d8093bf8e851b66da9b899b02ed6bb3424
BenceKercsi <78799553+KercsiBence@users.noreply.github.com>
2021-11-25 11:03:43 +0100

    Init commit with AttendanceSheetController test and MentorController test and AttendanceSheetController addPost name change

---
+++ EvoNaplo.IntegrationTest/AttendanceSheetControllerTest.cs
@@ -0,0 +1,109 @@
+﻿using EvoNaplo.Controllers;
+using EvoNaplo.DataAccessLayer;
+using EvoNaplo.Models;
+using EvoNaplo.Models.DTO;
+using EvoNaplo.Services;
+using EvoNaplo.TestHelper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoNaplo.IntegrationTest
+{
+    [TestFixture]
+    class AttendanceSheetControllerTest
+    {
+        AttendanceSheetController _attendanceSheetController;
+        EvoNaploContext _evoNaploContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //TODO: Populate database
+            _evoNaploContext = EvoNaploContextHelper.CreateInMemoryDatabaseContext();
+            AttendanceSheetService attendanceSheetService = new AttendanceSheetService(_evoNaploContext);
+
+            _attendanceSheetController = new AttendanceSheetController(attendanceSheetService);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _attendanceSheetController = null;
+        }
+
+        [Test]
+        public async Task GetAttendanceSheets_with_2_data()
+        {
+            //Arrange
+            Semester semester = new()
+            {
+                Id = 1,
+                StartDate = new DateTime(2016, 8, 21),
+                EndDate = new DateTime(2022, 9, 13),
+                IsAppliable = true
+            };
+            _evoNaploContext.Semesters.Add(semester);
+            Project project = new()
+            {
+                Id = 1,
+                ProjectName = "EvoNaplo",
+                Description = "Egy naplo",
+                SourceLink = "asd",
+                Technologies = "C#",
+                SemesterId = 1
+            };
+            _evoNaploContext.Projects.Add(project);
+
+            List<AttendanceSheet> sheets = new();
+            AttendanceSheet sheet1 = new()
+            {
+                Id = 1,
+                MeetingDate = new DateTime(2021, 10, 14),
+                ProjectId = 1
+
+            };
+            AttendanceSheet sheet2 = new()
+            {
+                Id = 2,
+                MeetingDate = new DateTime(2021, 11, 24),
+                ProjectId = 1
+            };
+            sheets.Add(sheet1);
+            sheets.Add(sheet2);
+            await _evoNaploContext.AttendanceSheets.AddRangeAsync(sheets);
+            _evoNaploContext.SaveChanges();
+
+            //Act
+            IEnumerable<AttendanceSheetDTO> actualSheets = _attendanceSheetController.GetAttendanceSheets();
+
+            //Assert
+            Assert.AreEqual(2, actualSheets.Count());
+
+        }
+        [Test]
+        public async Task AddAttendanceSheet_ValidData()
+        {
+            //Arrange
+            int expectedNumberOfAttendanceSheets = _evoNaploContext.AttendanceSheets.ToList().Count() + 1;
+            AttendanceSheet attendanceSheet = new()
+            {
+                Id = 3,
+                MeetingDate = new DateTime(2021, 6, 4),
+                ProjectId = 1
+            };
+
+            //Act
+            await _attendanceSheetController.PostAddAttendanceSheet(attendanceSheet);
+
+            //Assert
+            int actualNumberOfAttendanceSheets = _evoNaploContext.AttendanceSheets.ToList().Count();
+            Assert.AreEqual(expectedNumberOfAttendanceSheets, actualNumberOfAttendanceSheets);
+            Assert.True(_evoNaploContext.AttendanceSheets.Last() == attendanceSheet);
+
+        }
+    }
+}
---
+++ EvoNaplo.TestHelper/EvoNaploContextHelper.cs
@@ -0,0 +1,29 @@
+﻿using EvoNaplo.DataAccessLayer;
+using EvoNaplo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvoNaplo.TestHelper
+{
+    public static class EvoNaploContextHelper
+    {
+        public static EvoNaploContext CreateInMemoryDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<EvoNaploContext>()
+                        .UseInMemoryDatabase(databaseName: "Test")
+                        .Options;
+
+            EvoNaploContext evoNaploContext = new EvoNaploContext(options);
+            return evoNaploContext;
+        }
+
+        public static EvoNaploContext CreateDefaultUsers(this EvoNaploContext evoNaploContext, int numberOfUsers, User.RoleTypes roleTypes)
+        {
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                evoNaploContext.Users.Add(UserHelper.CreateDefaultUser(roleTypes));
+            }
+
+            return evoNaploContext;
+        }
+    }
+}
